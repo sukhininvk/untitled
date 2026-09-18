7e7f5e7d2655edbde975ad9b724e0ca3292c80f2
Vadim Sukhinin <sukhininvk@gmail.com>
2026-09-18 21:33:50 +1000

    Refactor preferences system; add section registration
    Redesign Preferences to support multiple sections via RegisterSection. Make Preferences non-copyable and non-movable. Update DisplayPreferences default resolution to 1280x720 and add SetDefaults() and CheckValid() methods. Remove global to_json/from_json for Preferences; manage sections internally. Add includes for <functional>, <string>, and <unordered_map>.

--- include/preferences.h
+++ include/preferences.h
@@ -3,6 +3,10 @@
 
 #include <nlohmann/json.hpp>
 
+#include <functional>
+#include <string>
+#include <unordered_map>
+
 namespace Untitled
 {
 	enum class DisplayMode
@@ -22,24 +26,72 @@ namespace Untitled
 
 	struct DisplayPreferences
 	{
-		int width = 2160;
-		int height = 1440;
+		int width = 1280;
+		int height = 720;
 		DisplayMode mode = DisplayMode::Windowed;
 		int max_fps = 0; // 0 = unlimited, otherwise specify the max framerate
 		VSyncMode vsync = VSyncMode::On;
+
+		void SetDefaults();
+		void CheckValid();
 	};
 
 	class Preferences
 	{
 	public:
+		Preferences() = default;
+		~Preferences() = default;
+
+		Preferences(const Preferences&) = delete;
+		Preferences& operator=(const Preferences&) = delete;
+
+		Preferences(Preferences&&) = delete;
+		Preferences& operator=(Preferences&&) = delete;
+
+		template<typename T>
+		bool RegisterSection(const std::string& name, T& preferences);
+
 		bool Load();
 		bool Save() const;
 
-		DisplayPreferences display;	
+		void SetDefaults();
+		void CheckValid();
+
+	private:
+		struct Section
+		{
+			std::function<void()> set_defaults;
+			std::function<void()> check_valid;
+
+			std::function<void(nlohmann::json&)> to_json;
+			std::function<void(const nlohmann::json&)> from_json;
+		};
+
+		std::unordered_map<std::string, Section> sections;
 	};
 
-	void to_json(nlohmann::json& j, const Preferences& p);
-	void from_json(const nlohmann::json& j, Preferences& p);
+	template<typename T>
+	bool Preferences::RegisterSection(const std::string& name, T& preferences)
+	{
+		if (name.empty())
+			return false;
+
+		if (sections.contains(name))
+			return false;
+
+		sections.emplace(
+			name,
+			Section
+			{
+				[&preferences]() { preferences.SetDefaults(); },
+				[&preferences]() { preferences.CheckValid(); },
+				[&preferences](nlohmann::json& j) { j = preferences; },
+				[&preferences](const nlohmann::json& j) { j.get_to(preferences); }
+			}
+		);
+
+		return true;
+	}
 }
 
 #endif
